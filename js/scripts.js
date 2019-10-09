cf59aa127397eb973c1b911665f0f931dc4d6151
Sylvain Tenier <sylvain_at_tenier_dot_name>
2019-10-09 21:04:01 +0200

    base code

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    // Le DOM est prêt, il est possible d'ajouter des écouteurs d'événements en toute sécurité
+    console.log("DOM entièrement chargé et analysé");
+  });
